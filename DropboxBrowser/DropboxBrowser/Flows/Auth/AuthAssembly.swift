92362311d57a624ba85d6e77a14f9d9083063122
Ostap Holub <holub.ostap@gmail.com>
2023-09-02 20:02:17 +0300

    Create AuthAssembly and connect it to app

---
+++ DropboxBrowser/DropboxBrowser/Flows/Auth/AuthAssembly.swift
@@ -0,0 +1,16 @@
+//
+//  AuthAssembly.swift
+//  DropboxBrowser
+//
+//  Created by Ostap Holub on 02.09.2023.
+//
+
+import Foundation
+import SwiftUI
+
+enum AuthAssembly {
+
+    static func makeModule() -> AnyView {
+        AnyView(Text("Auth module"))
+    }
+}
